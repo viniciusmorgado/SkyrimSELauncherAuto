2a8acb4a921175080caa7a2ff50cff56bd65007c
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-03-17 19:07:30 -0300

    Change the script for a more direct approach to execute skse64 skyrim launch, removing bat intermediary

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace SkyrimSELauncher
 {
     class Program
     {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
         static void Main(string[] args)
         {
             
             var startupPath = System.IO.Path.GetDirectoryName(
                 System.Reflection.Assembly.GetEntryAssembly().Location);
-            var programPath = System.IO.Path.Combine(startupPath, "SkyrimSELauncher_Exec");
+            var programPath = System.IO.Path.Combine(startupPath, "skse64_loader.exe");
 
             System.Diagnostics.Process.Start(programPath);
-            
+             IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+                ShowWindow(h, 0);
+                
+                while (true){
+                System.Threading.Thread.Sleep(1);
+            }
         }
     }
 }
