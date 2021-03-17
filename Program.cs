e2c58e1b6fce2efdc7b45611c2cec578144f56ff
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-03-17 18:54:10 -0300

    Add patch script

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SkyrimSELauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var startupPath = System.IO.Path.GetDirectoryName(
+                System.Reflection.Assembly.GetEntryAssembly().Location);
+            var programPath = System.IO.Path.Combine(startupPath, "SkyrimSELauncher_Exec");
+
+            System.Diagnostics.Process.Start(programPath);
+            
+        }
+    }
+}
