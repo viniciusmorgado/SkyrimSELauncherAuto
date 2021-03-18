dce0d8e43d1ac9005c12fda207e45c2208ddcbc6
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-03-18 02:21:59 -0300

    Better code identation

--- Program.cs
+++ Program.cs
@@ -10,18 +10,20 @@ namespace SkyrimSELauncher
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
         static void Main(string[] args)
         {
-            
-            var startupPath = System.IO.Path.GetDirectoryName(
-                System.Reflection.Assembly.GetEntryAssembly().Location);
+            var startupPath = System.IO.Path.GetDirectoryName
+            (
+                System.Reflection.Assembly.GetEntryAssembly().Location
+            );
             var programPath = System.IO.Path.Combine(startupPath, "skse64_loader.exe");
 
             System.Diagnostics.Process.Start(programPath);
-             IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
-                ShowWindow(h, 0);
+            IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(h, 0);
                 
-                while (true){
-                System.Threading.Thread.Sleep(1);
-            }
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(1);
+                }
         }
     }
 }
